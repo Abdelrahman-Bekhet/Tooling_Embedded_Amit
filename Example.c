36af9582919244111ca0b949ec37543c40200079
Abdelrahman Bekhet <abdelrahman.mbekhet@gmail.com>
2022-08-30 00:41:23 +0200

    Setting repo and Example

---
+++ Example.c
@@ -0,0 +1,9 @@
+#include"stdio.h"
+#include"Example.h"
+
+int main()
+{
+	print_my_name();
+	
+	return 0;
+}
---
+++ Func.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"Example.h"
+void print_my_name()
+{
+printf("My name is Abdelrahman \n");
+}
